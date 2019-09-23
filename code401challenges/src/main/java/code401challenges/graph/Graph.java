bd1d4959153cf50adfa8c2f1bde2c5ac3a35169e
trevorjdobson <trevorjdobson@gmail.com>
2019-09-23 11:40:38 -0700

    code challenge complete

--- code401challenges/src/main/java/code401challenges/graph/Graph.java
+++ code401challenges/src/main/java/code401challenges/graph/Graph.java
@@ -1,9 +1,6 @@
 package code401challenges.graph;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Graph<T> {
     public Map<Vertex<T>, ArrayList<Edge<T>>> adjacencyList;
@@ -11,6 +8,24 @@ public class Graph<T> {
 
     public Graph(){
         this.adjacencyList = new HashMap<Vertex<T>, ArrayList<Edge<T>>>();
+    }
+
+    public Set<Vertex> breadthFirst(Vertex root){
+        Set<Vertex> visited = new LinkedHashSet<>();
+        Queue<Vertex> queue = new LinkedList<>();
+        queue.add(root);
+        visited.add(root);
+        while(!queue.isEmpty()){
+            Vertex vertex = queue.poll();
+            List<Edge<T>> neighbors = this.getNeighbors(vertex);
+            for(Edge v : neighbors){
+                if(!visited.contains(v.vertex)){
+                    visited.add(v.vertex);
+                    queue.add(v.vertex);
+                }
+            }
+        }
+        return visited;
     }
 
     public Vertex<T> addNode(T vertex){
